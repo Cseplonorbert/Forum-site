bf85cdefc88f8244eee0387003ce2c4abc13c765
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-04 22:03:46 +0200

    QuestionController/ implement addAnswer method.

--- src/main/java/com/codecool/Forum/controller/QuestionController.java
+++ src/main/java/com/codecool/Forum/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.codecool.Forum.controller;
 
 import com.codecool.Forum.exception.QuestionNotFoundException;
 import com.codecool.Forum.model.Question;
+import com.codecool.Forum.service.AnswerService;
 import com.codecool.Forum.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,12 @@ import java.util.List;
 public class QuestionController {
 
     QuestionService questionService;
+    AnswerService answerService;
 
     @Autowired
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, AnswerService answerService) {
         this.questionService = questionService;
+        this.answerService = answerService;
     }
 
     @GetMapping
@@ -41,5 +44,18 @@ public class QuestionController {
     @PostMapping("add-question")
     public ResponseEntity<Question> addQuestion(@RequestParam String title, @RequestParam String description) {
         return ResponseEntity.status(HttpStatus.CREATED).body(questionService.addQuestion(title, description));
+    }
+
+    @PostMapping("/question/{question_id}/new-answer")
+    public ResponseEntity<Question> addAnswer(@RequestParam String message, @PathVariable Long question_id) {
+        try {
+            Question question = questionService.getQuestionById(question_id);
+            answerService.addNewAnswer(question, message);
+            return ResponseEntity.status(HttpStatus.CREATED).body(question);
+        } catch (QuestionNotFoundException exc) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Question Not Found", exc
+            );
+        }
     }
 }
