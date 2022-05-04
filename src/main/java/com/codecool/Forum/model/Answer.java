8fb0e00259f65ded80f53b23666ec0959060c380
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-04 22:02:12 +0200

    Implement Answer @Entity class.

---
+++ src/main/java/com/codecool/Forum/model/Answer.java
@@ -0,0 +1,33 @@
+package com.codecool.Forum.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ANSWERS")
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @JsonIgnore
+    @Builder.Default private LocalDateTime createdOn = LocalDateTime.now();
+    @Builder.Default private String formattedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+}
