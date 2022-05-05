de1347bcc5b35473e07f784086ecb1a5ef157b25
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-05 16:10:11 +0200

    AnswerService/ implement deleteAnswer method.

--- src/main/java/com/codecool/Forum/service/AnswerService.java
+++ src/main/java/com/codecool/Forum/service/AnswerService.java
@@ -1,10 +1,13 @@
 package com.codecool.Forum.service;
 
+import com.codecool.Forum.exception.AnswerNotFoundException;
 import com.codecool.Forum.model.Answer;
 import com.codecool.Forum.model.Question;
 import com.codecool.Forum.reporsitory.AnswerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AnswerService {
@@ -22,5 +25,15 @@ public class AnswerService {
                 .question(question)
                 .build();
         answerRepository.save(answer);
+    }
+
+    public Question deleteAnswer(Long id) {
+        Optional<Answer> answer = answerRepository.findById(id);
+        if (answer.isPresent()) {
+            Question question = answer.get().getQuestion();
+            answerRepository.delete(answer.get());
+            return question;
+        }
+        throw new AnswerNotFoundException("Answer Not Found");
     }
 }
