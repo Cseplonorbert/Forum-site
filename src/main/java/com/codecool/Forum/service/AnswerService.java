027ba05f7348f9a684a4c4b21098f88630e686ce
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-09 05:44:10 +0200

    AnswerService/ implement voteOnAnswer method.

--- src/main/java/com/codecool/Forum/service/AnswerService.java
+++ src/main/java/com/codecool/Forum/service/AnswerService.java
@@ -3,6 +3,7 @@ package com.codecool.Forum.service;
 import com.codecool.Forum.exception.AnswerNotFoundException;
 import com.codecool.Forum.model.Answer;
 import com.codecool.Forum.model.Question;
+import com.codecool.Forum.model.Vote;
 import com.codecool.Forum.reporsitory.AnswerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,8 +38,14 @@ public class AnswerService {
 
     public Question deleteAnswer(Long id) {
         Answer answer = getAnswerById(id);
-        Question question = answer.getQuestion();
         answerRepository.delete(answer);
-        return question;
+        return answer.getQuestion();
+    }
+
+    public Question voteOnAnswer(Long id, String vote) {
+        Answer answer = getAnswerById(id);
+        Vote voteDir = Vote.valueOf(vote);
+        answer.setNumberOfVotes(answer.getNumberOfVotes() + voteDir.getValue());
+        return answer.getQuestion();
     }
 }
