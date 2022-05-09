75aaa8d9b5d52beb4f51a00ab211af39ec39d965
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-09 17:52:14 +0200

    Implement TagRepository class.

---
+++ src/main/java/com/codecool/Forum/reporsitory/TagRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.Forum.reporsitory;
+
+import com.codecool.Forum.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
