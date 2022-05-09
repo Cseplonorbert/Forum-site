6aeb3f50827fa524c94f366a5531036d2a7ccd0d
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-09 12:34:41 +0200

    CommentService/ implement findCommentById method.

--- src/main/java/com/codecool/Forum/service/CommentService.java
+++ src/main/java/com/codecool/Forum/service/CommentService.java
@@ -1,11 +1,14 @@
 package com.codecool.Forum.service;
 
+import com.codecool.Forum.exception.CommentNotFoundException;
 import com.codecool.Forum.model.Answer;
 import com.codecool.Forum.model.Comment;
 import com.codecool.Forum.model.Question;
 import com.codecool.Forum.reporsitory.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -15,6 +18,14 @@ public class CommentService {
     @Autowired
     public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
+    }
+
+    public Comment findCommentById(Long id) {
+        Optional<Comment> comment = commentRepository.findById(id);
+        if (comment.isPresent()) {
+            return comment.get();
+        }
+        throw new CommentNotFoundException("Comment not found");
     }
 
     public void addNewComment(Question question, String message) {
