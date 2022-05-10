b345e79545c2dd3fbf8246057bfdd67aedee3aa0
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-10 06:26:59 +0200

    TagService/ implement add method.

--- src/main/java/com/codecool/Forum/service/TagService.java
+++ src/main/java/com/codecool/Forum/service/TagService.java
@@ -1,8 +1,14 @@
 package com.codecool.Forum.service;
 
+import com.codecool.Forum.exception.TagAlreadyAddedToQuestionException;
+import com.codecool.Forum.model.Question;
+import com.codecool.Forum.model.Tag;
 import com.codecool.Forum.reporsitory.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TagService {
@@ -12,5 +18,23 @@ public class TagService {
     @Autowired
     public TagService(TagRepository tagRepository) {
         this.tagRepository = tagRepository;
+    }
+
+    public Optional<Tag> getTagByName(String tagName) {
+        return tagRepository.findByName(tagName);
+    }
+
+    public void add(String tagName, Question question) {
+        Optional<Tag> tag = getTagByName(tagName);
+        if (!tag.isPresent()) {
+            tag = Optional.of(Tag.builder().name(tagName).build());
+        }
+        List<Question> questions = tag.get().getQuestions();
+        if (question.getTags().contains(tag.get())) {
+            throw new TagAlreadyAddedToQuestionException("Tag already added to the question");
+        }
+        questions.add(question);
+        tag.get().setQuestions(questions);
+        tagRepository.save(tag.get());
     }
 }
