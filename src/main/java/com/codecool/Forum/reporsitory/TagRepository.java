711930d6e4e91f18eb79379efe698deb95a5c4f2
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-10 06:10:53 +0200

    TagRepository/ add findByName method.

--- src/main/java/com/codecool/Forum/reporsitory/TagRepository.java
+++ src/main/java/com/codecool/Forum/reporsitory/TagRepository.java
@@ -3,6 +3,8 @@ package com.codecool.Forum.reporsitory;
 import com.codecool.Forum.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
+import java.util.Optional;
 
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
 }
