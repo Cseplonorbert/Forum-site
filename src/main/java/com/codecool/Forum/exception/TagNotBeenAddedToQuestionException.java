85cd83e8d75d288968139070ec78484cb2602940
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-10 08:19:01 +0200

    Implement TagNotBeenAddedToQuestionException.

---
+++ src/main/java/com/codecool/Forum/exception/TagNotBeenAddedToQuestionException.java
@@ -0,0 +1,7 @@
+package com.codecool.Forum.exception;
+
+public class TagNotBeenAddedToQuestionException extends RuntimeException{
+    public TagNotBeenAddedToQuestionException(String message) {
+        super(message);
+    }
+}
