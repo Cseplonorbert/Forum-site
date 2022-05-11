06b4034e9c973680ce042594ddb7860520d5415b
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-11 05:50:18 +0200

    QuestionRepository/ modify findAll method, now it returns a Page object, and take a Pageable argument.

--- src/main/java/com/codecool/Forum/reporsitory/QuestionRepository.java
+++ src/main/java/com/codecool/Forum/reporsitory/QuestionRepository.java
@@ -1,14 +1,14 @@
 package com.codecool.Forum.reporsitory;
 
 import com.codecool.Forum.model.Question;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    List<Question> findAll(Sort sort);
+    Page<Question> findAll(Pageable pageable);
     Optional<Question> findQuestionById(Long id);
 }
