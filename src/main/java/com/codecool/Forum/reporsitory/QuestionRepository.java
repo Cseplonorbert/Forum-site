2b59a2732d3895021b71f884393804d3c2ecc930
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-11 07:04:46 +0200

    QuestionRepository/ add findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase method.

--- src/main/java/com/codecool/Forum/reporsitory/QuestionRepository.java
+++ src/main/java/com/codecool/Forum/reporsitory/QuestionRepository.java
@@ -11,4 +11,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Page<Question> findAll(Pageable pageable);
     Optional<Question> findQuestionById(Long id);
+    Page<Question> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(Pageable pageable,
+                                                                                    String title,
+                                                                                    String description);
 }
