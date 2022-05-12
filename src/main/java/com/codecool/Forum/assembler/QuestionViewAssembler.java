71b43f8cd6b7e5f5ddf636985e7a6b6222ed488c
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-12 14:42:27 +0200

    Implement QuestionViewAssembler.

---
+++ src/main/java/com/codecool/Forum/assembler/QuestionViewAssembler.java
@@ -0,0 +1,35 @@
+package com.codecool.Forum.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.codecool.Forum.controller.QuestionController;
+import com.codecool.Forum.mapper.QuestionQuestionViewMapper;
+import com.codecool.Forum.model.Question;
+import com.codecool.Forum.model.view.QuestionView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionViewAssembler implements RepresentationModelAssembler<Question, EntityModel<QuestionView>> {
+
+    private final QuestionQuestionViewMapper questionQuestionViewMapper;
+
+    @Autowired
+    public QuestionViewAssembler(QuestionQuestionViewMapper questionQuestionViewMapper) {
+        this.questionQuestionViewMapper = questionQuestionViewMapper;
+    }
+
+
+    @Override
+    public EntityModel<QuestionView> toModel(Question question) {
+        QuestionView questionView = questionQuestionViewMapper.questionQuestionView(question);
+
+        return EntityModel.of(questionView,
+                linkTo(methodOn(QuestionController.class).getQuestionById(questionView.getId())).withSelfRel(),
+                linkTo(methodOn(QuestionController.class).getAnswers(questionView.getId())).withRel("answers"),
+                linkTo(methodOn(QuestionController.class).getComments(questionView.getId())).withRel("comments"),
+                linkTo(methodOn(QuestionController.class).getTags(questionView.getId())).withRel("tags"));
+    }
+}
