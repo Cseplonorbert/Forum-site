f093c3e3a18a8f81d68f1b90884f2aa3dedffe21
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-12 14:41:32 +0200

    QuestionController/ refactor: rename routes, create skeleton for use the route with new models.

--- src/main/java/com/codecool/Forum/controller/QuestionController.java
+++ src/main/java/com/codecool/Forum/controller/QuestionController.java
@@ -1,12 +1,15 @@
 package com.codecool.Forum.controller;
 
 import com.codecool.Forum.assembler.QuestionPreviewAssembler;
+import com.codecool.Forum.assembler.QuestionViewAssembler;
 import com.codecool.Forum.exception.*;
+import com.codecool.Forum.model.Answer;
 import com.codecool.Forum.model.Question;
 import com.codecool.Forum.model.QuestionPreview;
-import com.codecool.Forum.model.Tag;
-import com.codecool.Forum.service.AnswerService;
-import com.codecool.Forum.service.CommentService;
+import com.codecool.Forum.model.view.AnswerView;
+import com.codecool.Forum.model.view.CommentView;
+import com.codecool.Forum.model.view.QuestionView;
+import com.codecool.Forum.model.view.TagView;
 import com.codecool.Forum.service.QuestionService;
 import com.codecool.Forum.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,36 +17,32 @@ import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/questions")
 public class QuestionController {
 
     QuestionService questionService;
-    AnswerService answerService;
-    CommentService commentService;
     TagService tagService;
     QuestionPreviewAssembler questionPreviewAssembler;
+    QuestionViewAssembler questionViewAssembler;
 
     @Autowired
     public QuestionController(QuestionService questionService,
-                              AnswerService answerService,
-                              CommentService commentService,
                               TagService tagService,
-                              QuestionPreviewAssembler questionPreviewAssembler) {
+                              QuestionPreviewAssembler questionPreviewAssembler,
+                              QuestionViewAssembler questionViewAssembler) {
         this.questionService = questionService;
-        this.answerService = answerService;
-        this.commentService = commentService;
         this.tagService = tagService;
         this.questionPreviewAssembler = questionPreviewAssembler;
+        this.questionViewAssembler = questionViewAssembler;
     }
 
-    @GetMapping("/questions")
+    @GetMapping
     public CollectionModel<EntityModel<QuestionPreview>> all(
                                                         @RequestParam(defaultValue = "SUBMISSION_TIME") String sort,
                                                         @RequestParam(defaultValue = "DESC") String order,
@@ -77,10 +76,11 @@ public class QuestionController {
         }
     }
 
-    @GetMapping("/question/{question_id}")
-    public Question getQuestionById(@PathVariable Long question_id) {
+    @GetMapping("/{id}")
+    public EntityModel<QuestionView> getQuestionById(@PathVariable Long id) {
         try {
-            return questionService.getQuestionById(question_id);
+            Question question = questionService.getQuestionById(id);
+            return questionViewAssembler.toModel(question);
         } catch (QuestionNotFoundException exc) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Question Not Found", exc
@@ -88,110 +88,63 @@ public class QuestionController {
         }
     }
 
-    @PostMapping("add-question")
-    public ResponseEntity<Question> addQuestion(@RequestParam String title, @RequestParam String description) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(questionService.addQuestion(title, description));
+    @PostMapping("/add")
+    public void add(@RequestParam String title, @RequestParam String description) {
+
     }
 
-    @PostMapping("/question/{question_id}/new-answer")
-    public ResponseEntity<Question> addAnswer(@RequestParam String message, @PathVariable Long question_id) {
-        try {
-            Question question = questionService.getQuestionById(question_id);
-            answerService.addNewAnswer(question, message);
-            return ResponseEntity.status(HttpStatus.CREATED).body(question);
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Question Not Found", exc
-            );
-        }
+    @PutMapping("/{id}/edit")
+    public void edit(@PathVariable Long id,@RequestParam String title, @RequestParam String description) {
+
     }
 
-    @DeleteMapping("/question/{question_id}")
-    public ResponseEntity<Void> deleteQuestion(@PathVariable Long question_id) {
-        try {
-            questionService.deleteQuestionById(question_id);
-            return ResponseEntity.noContent().build();
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
     }
 
-    @PutMapping("/question/{question_id}/edit")
-    public ResponseEntity<Question> updateQuestion(@PathVariable Long question_id, @RequestParam String title,
-                                                   @RequestParam String description) {
-        try {
-            Question question = questionService.updateQuestion(question_id, title, description);
-            return ResponseEntity.status(HttpStatus.OK).body(question);
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        }
+    @PostMapping("/{id}/down_vote")
+    public void downVote(@PathVariable Long id) {
+
     }
 
-    @PutMapping("/question/{question_id}/vote")
-    public ResponseEntity<Question> voteOnQuestion(@PathVariable Long question_id,
-                                                     @RequestParam(name = "vote_method") String vote) {
-        try {
-            Question question = questionService.voteOnQuestion(question_id, vote);
-            return ResponseEntity.status(HttpStatus.OK).body(question);
-        } catch (QuestionNotFoundException | IllegalArgumentException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        }
+    @PostMapping("/{id}/up_vote")
+    public void upVote(@PathVariable Long id) {
+
     }
 
-    @PostMapping("/question/{question_id}/new-comment")
-    public ResponseEntity<Question> addComment(@RequestParam String message,
-                                               @PathVariable Long question_id) {
-        try {
-            Question question = questionService.getQuestionById(question_id);
-            commentService.addNewComment(question, message);
-            return ResponseEntity.status(HttpStatus.CREATED).body(question);
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        }
+    @GetMapping("/{id}/answers")
+    public CollectionModel<EntityModel<AnswerView>> getAnswers(@PathVariable Long id) {
+        return CollectionModel.empty();
     }
 
-    @PostMapping("/question/{question_id}/new-tag")
-    public ResponseEntity<Question> addTag(@PathVariable Long question_id,
-                                           @RequestParam String tagName) {
-        try {
-            Question question = questionService.getQuestionById(question_id);
-            tagService.add(tagName, question);
-            return ResponseEntity.status(HttpStatus.CREATED).body(questionService.getQuestionById(question_id));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        } catch (TagAlreadyAddedToQuestionException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, exc.getMessage(), exc
-            );
-        }
+    @PostMapping("/{id}/answers/add")
+    public void addAnswer(@PathVariable Long id, @RequestParam String message) {
+
     }
 
-    @DeleteMapping("/question/{question_id}/tag/{tag_id}")
-    public ResponseEntity<Question> removeTagFromQuestion(@PathVariable Long question_id,
-                                                          @PathVariable Long tag_id) {
-        try {
-            Question question = questionService.getQuestionById(question_id);
-            Tag tag = tagService.getTagById(tag_id);
-            tagService.removeTagFromQuestion(tag, question);
-            return ResponseEntity.status(HttpStatus.OK).body(questionService.getQuestionById(question_id));
-        } catch (QuestionNotFoundException | TagNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        } catch (TagNotBeenAddedToQuestionException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, exc.getMessage(), exc
-            );
-        }
+    @GetMapping("/{id}/comments")
+    public CollectionModel<EntityModel<CommentView>> getComments(@PathVariable Long id) {
+        return CollectionModel.empty();
+    }
+
+    @PostMapping("/{id}/comments/add")
+    public void addComment(@PathVariable Long id, @RequestParam String message) {
+
+    }
+
+    @GetMapping("/{id}tags")
+    public CollectionModel<EntityModel<TagView>> getTags(@PathVariable Long id) {
+        return CollectionModel.empty();
+    }
+
+    @PostMapping("/{id}/tags/add")
+    public void addTag(@PathVariable Long id, @RequestParam String tagName) {
+
+    }
+
+    @DeleteMapping("/{id}/tags/delete/{tag_id}")
+    public void deleteTag(@PathVariable Long id, @PathVariable Long tag_id) {
+
     }
 }
