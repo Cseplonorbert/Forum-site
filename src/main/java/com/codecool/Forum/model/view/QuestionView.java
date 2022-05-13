1acf3ffa531d8b405143638f9f77ebe0cda2be68
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-13 12:51:16 +0200

    QuestionView/ add JsonIgnore to id field.

--- src/main/java/com/codecool/Forum/model/view/QuestionView.java
+++ src/main/java/com/codecool/Forum/model/view/QuestionView.java
@@ -1,5 +1,6 @@
 package com.codecool.Forum.model.view;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Builder
 public class QuestionView {
+    @JsonIgnore
     private Long id;
     private String title;
     private String description;
