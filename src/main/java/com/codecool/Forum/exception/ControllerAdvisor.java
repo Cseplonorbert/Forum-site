7ea5a45befea783bf0d6def247796aeff1723211
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-24 09:39:53 +0200

    ControllerAdvisor/ set up tag related exceptions.

--- src/main/java/com/codecool/Forum/exception/ControllerAdvisor.java
+++ src/main/java/com/codecool/Forum/exception/ControllerAdvisor.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -15,7 +14,7 @@ import java.util.Map;
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(QuestionNotFoundException.class)
-    public ResponseEntity<Object> handleQuestionNotFoundException(QuestionNotFoundException exc, WebRequest request) {
+    public ResponseEntity<Object> handleQuestionNotFoundException(QuestionNotFoundException exc) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamps", LocalDateTime.now());
         body.put("message", exc.getMessage());
@@ -24,7 +23,7 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(AnswerNotFoundException.class)
-    public ResponseEntity<Object> handleAnswerNotFoundException(AnswerNotFoundException exc, WebRequest request) {
+    public ResponseEntity<Object> handleAnswerNotFoundException(AnswerNotFoundException exc) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamps", LocalDateTime.now());
         body.put("message", exc.getMessage());
@@ -33,7 +32,34 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
-    public  ResponseEntity<Object> handleCommentNotFoundException(CommentNotFoundException exc, WebRequest request) {
+    public  ResponseEntity<Object> handleCommentNotFoundException(CommentNotFoundException exc) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamps", LocalDateTime.now());
+        body.put("message", exc.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TagAlreadyAddedToQuestionException.class)
+    public ResponseEntity<Object> handleTagAlreadyAddedToQuestionException(TagAlreadyAddedToQuestionException exc) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamps", LocalDateTime.now());
+        body.put("message", exc.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TagNotBeenAddedToQuestionException.class)
+    public ResponseEntity<Object> handleTagNotBeenAddedToQuestionException(TagNotBeenAddedToQuestionException exc) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamps", LocalDateTime.now());
+        body.put("message", exc.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TagNotFoundException.class)
+    public ResponseEntity<Object> handleTagNotFoundException(TagNotFoundException exc) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamps", LocalDateTime.now());
         body.put("message", exc.getMessage());
--- src/main/java/com/codecool/Forum/exception/TagNotBeenAddedToQuestionException.java
+++ src/main/java/com/codecool/Forum/exception/TagNotBeenAddedToQuestionException.java
@@ -1,7 +1,7 @@
 package com.codecool.Forum.exception;
 
 public class TagNotBeenAddedToQuestionException extends RuntimeException{
-    public TagNotBeenAddedToQuestionException(String message) {
-        super(message);
+    public TagNotBeenAddedToQuestionException(Long questionId, Long tagId) {
+        super(String.format("Tag with id %d has not been added question with %d id", tagId, questionId));
     }
 }
