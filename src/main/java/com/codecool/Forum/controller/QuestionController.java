d45f2a68d564646af59565c3912d849263333ef9
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-24 07:23:01 +0200

    Refactor: QuestionController/ delete exception handling for QuestionNotFoundException because the usage of advisor.

--- src/main/java/com/codecool/Forum/controller/QuestionController.java
+++ src/main/java/com/codecool/Forum/controller/QuestionController.java
@@ -4,7 +4,6 @@ import com.codecool.Forum.assembler.*;
 import com.codecool.Forum.exception.*;
 import com.codecool.Forum.model.*;
 import com.codecool.Forum.model.view.*;
-import com.codecool.Forum.service.AnswerService;
 import com.codecool.Forum.service.CommentService;
 import com.codecool.Forum.service.QuestionService;
 import com.codecool.Forum.service.TagService;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/api")
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -28,8 +27,6 @@ public class QuestionController {
     private final QuestionPreviewAssembler questionPreviewAssembler;
     private final QuestionViewAssembler questionViewAssembler;
     private final PagedResourcesAssembler<Question> pagedResourcesAssembler;
-    private final AnswerViewAssembler answerViewAssembler;
-    private final AnswerService answerService;
     private final CommentService commentService;
     private final CommentViewAssembler commentViewAssembler;
     private final TagViewAssembler tagViewAssembler;
@@ -40,8 +37,6 @@ public class QuestionController {
                               QuestionPreviewAssembler questionPreviewAssembler,
                               QuestionViewAssembler questionViewAssembler,
                               PagedResourcesAssembler<Question> pagedResourcesAssembler,
-                              AnswerViewAssembler answerViewAssembler,
-                              AnswerService answerService,
                               CommentService commentService,
                               CommentViewAssembler commentViewAssembler,
                               TagViewAssembler tagViewAssembler) {
@@ -50,14 +45,12 @@ public class QuestionController {
         this.questionPreviewAssembler = questionPreviewAssembler;
         this.questionViewAssembler = questionViewAssembler;
         this.pagedResourcesAssembler = pagedResourcesAssembler;
-        this.answerViewAssembler = answerViewAssembler;
-        this.answerService = answerService;
         this.commentService = commentService;
         this.commentViewAssembler = commentViewAssembler;
         this.tagViewAssembler = tagViewAssembler;
     }
 
-    @GetMapping
+    @GetMapping("/questions")
     public PagedModel<EntityModel<QuestionPreview>> all(
                                                         @RequestParam(defaultValue = "SUBMISSION_TIME") String sort,
                                                         @RequestParam(defaultValue = "DESC") String order,
@@ -72,7 +65,7 @@ public class QuestionController {
         }
     }
 
-    @GetMapping("/search")
+    @GetMapping("/questions/search")
     public  PagedModel<EntityModel<QuestionPreview>> search(
                                                         @RequestParam(defaultValue = "SUBMISSION_TIME") String sort,
                                                         @RequestParam(defaultValue = "DESC") String order,
@@ -88,19 +81,12 @@ public class QuestionController {
         }
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/questions/{id}")
     public EntityModel<QuestionView> get(@PathVariable Long id) {
-        try {
-            Question question = questionService.getQuestionById(id);
-            return questionViewAssembler.toModel(question);
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, exc.getMessage(), exc
-            );
-        }
+            return questionViewAssembler.toModel(questionService.getQuestionById(id));
     }
 
-    @PostMapping("/add")
+    @PostMapping("/questions/add")
     public ResponseEntity<EntityModel<QuestionView>> add(@RequestBody Question question) {
             return ResponseEntity
                     .status(HttpStatus.CREATED)
@@ -108,111 +94,67 @@ public class QuestionController {
                             .toModel(questionService.add(question)));
     }
 
-    @PutMapping("/{id}/edit")
+    @PutMapping("/questions/{id}/edit")
     public ResponseEntity<EntityModel<QuestionView>> update(@PathVariable Long id, Question question) {
-        try {
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(questionViewAssembler
                             .toModel(questionService.update(id, question)));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/questions/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        try {
             questionService.delete(id);
             return ResponseEntity.noContent().build();
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @PostMapping("/{id}/down_vote")
+    @PostMapping("/questions/{id}/down_vote")
     public EntityModel<QuestionView> downVote(@PathVariable Long id) {
-        try {
             return questionViewAssembler.toModel(questionService.downVote(id));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @PostMapping("/{id}/up_vote")
+    @PostMapping("/questions/{id}/up_vote")
     public EntityModel<QuestionView> upVote(@PathVariable Long id) {
-        try {
             return questionViewAssembler.toModel(questionService.upVote(id));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @GetMapping("/{id}/answers")
-    public CollectionModel<EntityModel<AnswerView>> getAnswers(@PathVariable Long id) {
-        return answerViewAssembler.toCollectionModel(answerService.getAnswersByQuestionId(id));
-    }
-
-    @PostMapping("/{id}/answers/add")
-    public ResponseEntity<EntityModel<AnswerView>> addAnswer(@PathVariable Long id, @RequestBody Answer answer) {
-        try {
-            Question question = questionService.getQuestionById(id);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(answerViewAssembler
-                            .toModel(answerService.add(question, answer)));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
-    }
-
-    @GetMapping("/{id}/comments")
+    @GetMapping("/questions/{id}/comments")
     public CollectionModel<EntityModel<CommentView>> getComments(@PathVariable Long id) {
         return commentViewAssembler.toCollectionModel(commentService.getCommentsByQuestionId(id));
     }
 
-    @PostMapping("/{id}/comments/add")
+    @PostMapping("/questions/{id}/comments/add")
     public ResponseEntity<EntityModel<CommentView>> addComment(@PathVariable Long id, @RequestBody Comment comment) {
-        try {
             Question question = questionService.getQuestionById(id);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(commentViewAssembler.toModel(commentService.add(question, comment)));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @GetMapping("/{id}tags")
+    @GetMapping("/questions/{id}tags")
     public CollectionModel<EntityModel<TagView>> getTags(@PathVariable Long id) {
-        try {
             Question question = questionService.getQuestionById(id);
             return tagViewAssembler.toCollectionModel(tagService.getTagsByQuestion(question));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
-        }
     }
 
-    @PostMapping("/{id}/tags/add")
+    @PostMapping("/questions/{id}/tags/add")
     public EntityModel<TagView> addTag(@PathVariable Long id, @RequestParam String tagName) {
         try {
             Question question = questionService.getQuestionById(id);
             return tagViewAssembler.toModel(tagService.add(tagName, question));
-        } catch (QuestionNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
         } catch (TagAlreadyAddedToQuestionException exc) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exc.getMessage(), exc);
         }
     }
 
-    @DeleteMapping("/{id}/tags/delete/{tag_id}")
+    @DeleteMapping("/questions/{id}/tags/delete/{tag_id}")
     public ResponseEntity<Void> deleteTag(@PathVariable Long id, @PathVariable Long tag_id) {
         try {
             Question question = questionService.getQuestionById(id);
             Tag tag = tagService.getTagById(tag_id);
             tagService.removeTagFromQuestion(tag, question);
             return ResponseEntity.noContent().build();
-        } catch (QuestionNotFoundException | TagNotFoundException exc) {
+        } catch (TagNotFoundException exc) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, exc.getMessage(), exc);
         } catch (TagNotBeenAddedToQuestionException exc) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exc.getMessage(), exc);
