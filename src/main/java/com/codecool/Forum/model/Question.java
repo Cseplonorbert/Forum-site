7e14186f32b96e8458f42ced89b7a213ddfd5515
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-27 11:47:52 +0200

    Refactor: Question/ remove @Builder, delete equals and hashcode methods.

--- src/main/java/com/codecool/Forum/model/Question.java
+++ src/main/java/com/codecool/Forum/model/Question.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "question")
@@ -26,43 +25,20 @@ public class Question {
     private String message;
 
     @OneToMany(mappedBy = "question", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-    @Builder.Default List<Answer> answers = new ArrayList<>();
+    List<Answer> answers = new ArrayList<>();
 
     @OneToMany(mappedBy = "question", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
-    @Builder.Default List<Comment> comments = new ArrayList<>();
+    List<Comment> comments = new ArrayList<>();
 
-    @ManyToMany
-    @JoinTable(
-            name = "question_tag",
-            joinColumns = @JoinColumn(name = "question_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id"))
-    @Builder.Default List<Tag> tags = new ArrayList<>();
+    @ManyToMany(mappedBy = "questions")
+    List<Tag> tags = new ArrayList<>();
 
     @Column(name = "view_number")
-    @Builder.Default private int viewNumber = 0;
+    private int viewNumber = 0;
 
     @Column(name = "vote_number")
-    @Builder.Default private int voteNumber = 0;
+    private int voteNumber = 0;
 
     @Column(name = "submission_time")
-    @Builder.Default private LocalDateTime submissionTime = LocalDateTime.now();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (!(o instanceof Question))
-            return false;
-
-        Question other = (Question) o;
-
-        return id != null &&
-                id.equals(other.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
-
+    private LocalDateTime submissionTime = LocalDateTime.now();
 }
