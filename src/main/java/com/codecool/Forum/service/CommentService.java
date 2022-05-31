33cc1b7b1cdafcaadff7abd632f2e4abd4ad1883
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-31 22:40:04 +0200

    Fix: CommentService throws QuestionNotFoundException at getCommentsByQuestionId, if the Question doesn't exist.

--- src/main/java/com/codecool/Forum/service/CommentService.java
+++ src/main/java/com/codecool/Forum/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.codecool.Forum.service;
 
 import com.codecool.Forum.exception.CommentNotFoundException;
+import com.codecool.Forum.exception.QuestionNotFoundException;
 import com.codecool.Forum.mapper.CommentDtoMapper;
 import com.codecool.Forum.model.Comment;
 import com.codecool.Forum.model.dto.AnswerGetDto;
@@ -44,6 +45,9 @@ public class CommentService {
     }
 
     public List<CommentGetDto> getCommentsByQuestionId(Long questionId) {
+        if (!questionService.existsById(questionId)) {
+            throw new QuestionNotFoundException(questionId);
+        }
         return commentRepository.findCommentsByQuestionId(questionId)
                 .stream().map(commentDtoMapper::commentToCommentGetDto).collect(Collectors.toList());
     }
